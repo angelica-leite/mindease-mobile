545a3efc788bca2fe707a873c462c5f867ce2896
Angélica Silva Leite <angelica.leite@cebraspe.org.br>
2026-03-07 01:14:09 -0300

    chore: arquivos de configuração

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./hooks/**/*.{js,jsx,ts,tsx}",
+    "./constants/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
